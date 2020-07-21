030e5afca093e820833fcade31228d81c36a25b7
Christiansen <me@martinmch.com>
2020-07-21 20:24:45 +0200

    Clean up

---
+++ drink
@@ -0,0 +1,3 @@
+#/usr/bin/env bash
+
+/usr/bin/notify-send "Drink!" "You should finish in 20 minutes."
--- drink.sh
+++
@@ -1,3 +0,0 @@
-#/usr/bin/env bash
-
-/usr/bin/notify-send "Drink!" "You should finish in 20 minutes."
